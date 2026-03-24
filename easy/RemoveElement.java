895fd45065ed2dc5e8b661fc69ec9fc5c2ff089a
Viet Hoang <gvhoang30112004@gmail.com>
2026-03-24 13:39:46 +0700

    Add RemoveElement solution implementation

---
+++ easy/RemoveElement.java
@@ -0,0 +1,27 @@
+package easy;
+//Given an integer array nums and an integer val, remove all occurrences of val in nums in-place.
+//The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+//Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+//Change the array nums such that the first k elements of nums contain the elements which are not equal to val.
+//The remaining elements of nums are not important as well as the size of nums.
+//Return k.
+public class RemoveElement {
+
+    public int removeElement(int[] nums, int val) {
+        int l = 0;
+        for ( int r = 0; r < nums.length; r++ ){
+            if ( nums[r] != val ){
+                nums[l++] = nums[r];
+            }
+        }
+        return l;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0,1,2,2,3,0,4,2};
+        System.out.println( new RemoveElement().removeElement(nums, 2));
+        for ( int num : nums ){
+            System.out.print(num + " ");
+        }
+    }
+}
